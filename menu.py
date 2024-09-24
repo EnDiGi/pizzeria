ac8422bb0a3b5decad3916bb5274b5f33dfe614a
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-09-24 14:36:20 +0200

    Creato menu.py

---
+++ menu.py
@@ -0,0 +1,27 @@
+pizze = [
+	'margherita',
+	'capricciosa',
+	'marinara',
+	'quattro formaggi',
+	'wurstel e patatine'
+]
+
+panini = [
+	'wurstel',
+	'cotoletta'
+]
+
+bevande = [
+	'acqua naturale',
+	'acqua frizzante',
+	'soda',
+	'aranciata'
+]
+
+bevande18 = [
+	'vino',
+	'birra',
+	'caipirinha'
+]
+
+menu = [pizze, panini, bevande]
