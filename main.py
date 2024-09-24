4f80d65444c36b58d14adc2f52f134b5c7e9bf16
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-09-24 14:33:47 +0200

    Aggiornato main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-nomePizzeria = 'nomegenericochecambieròappenativedròetroveraiunnomemigliore'
+import menu
+
+nomePizzeria = 'Pac-Man Pizza'
 
 def accogliCliente():
 
-	nome = ''
-	età = 0
+        nome = ''
+        età = 0
 
-	print('Benvenuto nella pizzeria', nomePizzeria + '!')
+        print('Benvenuto nella pizzeria ' + nomePizzeria + '!')
 
-	return nome, età
+        return nome, età
 
-def faiordine(nome, età):
-	ordine = []
-
-	return ordine
+def faiOrdine(nome, età):
+        ordine = []
+		
+        if età >= 18:
+                menu.bevande += menu.bevande18
+		
+        print(menu.menu)
+		
+        return ordine
 
 def mostraOrdine(nome, ordine):
-	pass
+        pass
 
 def main():
-	nome, età = accogliCliente()
-	ordine = faiordine(nome, età)
-	mostraOrdine(nome, ordine)
+        nome, età = accogliCliente()
+        ordine = faiOrdine(nome, età)
+        mostraOrdine(nome, ordine)
 
 if __name__ == '__main__':
-	main()
+        main()
